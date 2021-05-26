07801e1d53656da1c1673ba17c6304e6585401eb
Amita Kunwar <amita.kunwar25@outlook.com>
2021-05-26 14:51:19 -0400

    Adding Controller

---
+++ MicroServicesProject/Services/Catalog/Catalog.API/Repositories/ProductRepositories.cs
@@ -0,0 +1,69 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entites;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepositories : IProductRepository
+    {
+
+        private readonly ICatalogContext _catalogContext;
+
+        public ProductRepositories(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext ?? throw new ArgumentNullException(nameof(catalogContext));
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _catalogContext
+                            .Products
+                            .Find(p => true).ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _catalogContext
+                     .Products
+                     .Find(p =>p.Id ==id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name,name);
+
+            return await _catalogContext
+                                .Products
+                                .Find(filter)
+                                .ToListAsync();
+           }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+            return await _catalogContext
+                                  .Products
+                                  .Find(filter)
+                                  .ToListAsync();
+
+        }
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
